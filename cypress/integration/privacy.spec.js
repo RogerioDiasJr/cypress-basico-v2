7fb8ae0e2e5106400a4c30cac934c079ec19e402
RogerioDiasJr <RogerioDiasJr>
2022-04-02 15:46:01 -0300

    changed2

--- cypress/integration/privacy.spec.js
+++ cypress/integration/privacy.spec.js
@@ -1,5 +1,4 @@
-it('testa a página da política de privavidade de forma independente', function() {
-  cy.visit('./src/privacy.html')
-
-  cy.contains('Talking About Testing').should('be.visible')
+it.only('testa a página da política de privavidade de forma independente', function() {
+    cy.visit('./src/privacy.html')
+    cy.contains('CAC TAT - Política de privacidade').should('be.visible')
 })
